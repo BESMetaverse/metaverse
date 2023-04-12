0d65fa3163e8aa915aa0c7ad45c97c79c07930a6
waqashakeem <waqas@rnssol.com>
2023-04-12 13:54:47 +0500

    update: Jenkins and Docker files added

---
+++ jenkins/scripts/docker-build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Default git commit number
+GIT_COMMIT=unspecified
+
+# Get current get commit number
+LABEL=$(git log -1 --format=%h) 
+echo "Build docker image with label "$LABEL
+
+# Build docker of current directory
+docker build  --build-arg NEXT_PUBLIC_API_KEY=$NEXT_PUBLIC_API_KEY --build-arg NEXT_PUBLIC_PROJECT_ID=$NEXT_PUBLIC_PROJECT_ID -t rnssolutions/besmetaverse-frontend:$LABEL .
