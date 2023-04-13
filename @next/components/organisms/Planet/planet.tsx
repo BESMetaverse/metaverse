7fb48f8292db37d9308c91cfd61029986573a44d
Zabeeh Ullah <zabeeh.ullah@rnssol.com>
2023-04-13 12:00:03 +0500

    Initial Zoom issue update: status(in progress)

--- @next/components/organisms/Planet/planet.tsx
+++ @next/components/organisms/Planet/planet.tsx
@@ -1,25 +1,28 @@
-import { useState, useEffect, useRef } from 'react'
+/* eslint-disable react/display-name */
+/* eslint-disable @typescript-eslint/promise-function-async */
+import { useState, useEffect, useRef, forwardRef } from 'react'
 import axios from 'axios'
 import { PlanetsDrawer } from '@molecules'
 import dynamic from 'next/dynamic'
-const Globe = dynamic(import('react-globe.gl'), { ssr: false })
+import { GlobeMethods } from 'react-globe.gl'
 
 const COLORS = ['#6898FF', '#A45CFF', '#D72E2E']
 
 export const Planet = (): JSX.Element => {
-  const globeEl = useRef(undefined)
+  const globeEl = useRef<GlobeMethods>()
   const [showLand, setShowLand] = useState({ right: false })
   const [countries, setCountries] = useState<{ features: any } | null>(null)
 
-  // useEffect((): any => {
-  //   if (globeEl.current) {
-  //     return (globeEl.current as GlobeMethods)?.pointOfView({
-  //       lat: 42,
-  //       lng: -95,
-  //       altitude: 0.9
-  //     })
-  //   }
-  // }, [])
+  useEffect((): any => {
+    if (globeEl.current) {
+      console.log('Test: ', globeEl)
+      return globeEl.current.pointOfView?.({
+        lat: 42,
+        lng: -95,
+        altitude: 0.5
+      })
+    }
+  }, [globeEl.current])
 
   useEffect(() => {
     const fetchData = async (): Promise<void> => {
@@ -53,10 +56,13 @@ export const Planet = (): JSX.Element => {
       <Globe
         polygonAltitude={30}
         ref={globeEl}
-        globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
+        // globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+        bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+        backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
         hexPolygonsData={countries?.features}
         hexPolygonResolution={2}
-        hexPolygonMargin={0.3}
+        hexPolygonMargin={0.1}
         hexPolygonColor={() => `${COLORS[Math.round(Math.random() * 2)]}`}
         onHexPolygonClick={handlePlotClick}
       />
@@ -64,3 +70,26 @@ export const Planet = (): JSX.Element => {
     </>
   )
 }
+
+// const Globe = dynamic(import('react-globe.gl'), { ssr: false })
+
+// const GlobeCustom = forwardRef((props: any, ref) => (
+//   <Globe {...props} forwardRef={ref} />
+// ))
+
+// const GlobeCustom = dynamic(
+//   async () => await import('@next/components/organisms/Globe'),
+//   {
+//     ssr: false
+//   }
+// )
+const GlobeCustom = dynamic(
+  () => import('../Globe').then((mod) => mod.GlobeCustom),
+  {
+    ssr: false
+  }
+)
+
+const Globe = forwardRef((props: any, ref) => (
+  <GlobeCustom {...props} forwardRef={ref} />
+))
--- @next/components/organisms/index.ts
+++ @next/components/organisms/index.ts
@@ -1,2 +1,3 @@
 export * from './AllowMintCard'
 export * from './Planet'
+export * from './Globe'
