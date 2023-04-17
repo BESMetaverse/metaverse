826390c44cc85e1cbedaf962783990d7cda36014
Ahmad <ahmad.nawaz@rnssol.com>
2023-04-17 21:47:51 +0500

    Remove: remove useless code

--- @next/components/pages/index.ts
+++ @next/components/pages/index.ts
@@ -1,4 +1,3 @@
 export * from './wallet-page'
 export * from './metaverse-page'
 export * from './home-page'
-export * from './xbull'
--- pages/xbull/index.ts
+++
@@ -1,3 +0,0 @@
-import { xBullPage } from '@next/components/pages/xbull'
-// import { GetServerSideProps } from 'next'
-export default xBullPage
